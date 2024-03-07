223f13e23505a3414d814c1200172e7fcab35f69
z31430158@gmail.com <zarmanimran@gmail.com>
2024-03-07 05:36:16 +0500

    backend commit

--- src/Context/FeedbackContext.jsx
+++ src/Context/FeedbackContext.jsx
@@ -1,26 +1,43 @@
-import { createContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { createContext, useEffect, useState } from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    // {
-    //   id: 1,
-    //   text: 'This is from context',
-    //   rating: 5,
-    // },
-  ]);
+  const [feedback, setFeedback] = useState([]);
 
-  const deleteFeedback = (id) => {
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
+
+    const data = await response.json();
+    setFeedback(data);
+  };
+
+  //? delete feedback
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you wanna delete this?')) {
+      await fetch(`/feedback/${id}`, {
+        method: 'DELETE',
+      });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
+  //? add feedback
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   return (
